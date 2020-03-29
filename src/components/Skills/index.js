61654ee27a9743b81384496865c62b6a843ca91e
Tom Brophy <t.brophy@gmail.com>
2020-03-29 23:03:58 +0800

    removed legend from skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -44,59 +44,6 @@ class Skills extends Component {
             <h1>Tom Brophy's Skills</h1>
           </div>
           {SkillsContainerComponent}
-          <div className='legend-container'>
-            <h3>Legend</h3>
-            <div className='skill'>
-              <div className = 'skill-level-container'>
-                <div className='skill-point' style={styleYes}></div>
-                <div className='skill-point' style={styleNo}></div>
-                <div className='skill-point' style={styleNo}></div>
-                <div className='skill-point' style={styleNo}></div>
-                <div className='skill-point' style={styleNo}></div>
-              </div>
-              <div className='legend-description'><strong>Cursory Knowledge</strong> read about it somewhere and understand how it works but limited/no experience using it</div>
-            </div>
-            <div className='skill'>
-              <div className = 'skill-level-container'>
-                <div className='skill-point' style={styleYes}></div>
-                <div className='skill-point' style={styleYes}></div>
-                <div className='skill-point' style={styleNo}></div>
-                <div className='skill-point' style={styleNo}></div>
-                <div className='skill-point' style={styleNo}></div>
-              </div>
-              <div className='legend-description'><strong>Basic Proficiency</strong> have some experience using the technology and good understanding of fundamental principals.  Would take some time to achieve Professional Expertise</div>
-            </div>
-            <div className='skill'>
-              <div className = 'skill-level-container'>
-                <div className='skill-point' style={styleYes}></div>
-                <div className='skill-point' style={styleYes}></div>
-                <div className='skill-point' style={styleYes}></div>
-                <div className='skill-point' style={styleNo}></div>
-                <div className='skill-point' style={styleNo}></div>
-              </div>
-              <div className='legend-description'><strong>Proficient</strong> considerable understanding of technology, knowledge of nuances, but either a lack of professional experience or a significant period of time since I last used the technology.  Could quickly reach Professional Expertise</div>
-            </div>
-            <div className='skill'>
-              <div className = 'skill-level-container'>
-                <div className='skill-point' style={styleYes}></div>
-                <div className='skill-point' style={styleYes}></div>
-                <div className='skill-point' style={styleYes}></div>
-                <div className='skill-point' style={styleYes}></div>
-                <div className='skill-point' style={styleNo}></div>
-              </div>
-              <div className='legend-description'><strong>Professional Expertise</strong> full understanding of technology, recent and significant professional experience using it.  Could use in a professional setting immediately including providing instruction or help to other developers.</div>
-            </div>
-            <div className='skill'>
-              <div className = 'skill-level-container'>
-                <div className='skill-point' style={styleYes}></div>
-                <div className='skill-point' style={styleYes}></div>
-                <div className='skill-point' style={styleYes}></div>
-                <div className='skill-point' style={styleYes}></div>
-                <div className='skill-point' style={styleYes}></div>
-              </div>
-              <div className='legend-description'><strong>Industry Leader</strong>  A founder, creator or contributor of the technology.  Could give lectures or talks to the general public on the topic.  Able to fix bugs or expand features in the technology itself.</div>
-            </div>
-          </div>
         </div>
       </div>
     );
